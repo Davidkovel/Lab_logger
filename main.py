11f0834afbd0f34891720da9a801137748394359
David <dkovel7@gmail.com>
2023-05-23 18:21:59 +0100

    task 2

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # task 2
+
+import logging
+
+
+def write_file(fill_path, data):
+    try:
+        with open(fill_path, "w") as file:
+            logger = logging.getLogger("Calculator")
+            logger.setLevel(logging.INFO)
+            logger.addHandler(logging.StreamHandler())
+            file.write(data)
+            logger.info("Успiшно записали в файл")
+    except Exception as ex:
+        logger.error(f"В вашем файлу {fill_path} трапилось помилка {ex}")
+
+
+logging.basicConfig(level=logging.INFO)
+write_file("output.txt", input("Введiть що треба вписати файл: "))
